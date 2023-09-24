fe26028903eb93461531a10e478c6feffc02c0b2
Grish Mahat <grishmahat@gmail.com>
2023-09-24 12:01:48 +0545

    try to improve it

--- README.md
+++ README.md
@@ -1,3 +1,48 @@
 # Staff Promotion System
-This system was originally made for Gamepixel and still is in use, I decided to make this public so other non hypixel remakes or remakes can use this
-🖖🇵🇰
+
+The Staff Promotion System is a tool that allows organizations to calculate and manage staff promotions based on specified criteria. This system is designed to provide transparency and consistency in the promotion process.
+
+## Features
+
+- Calculate promotion eligibility based on points and criteria.
+- Generate personalized promotion messages.
+- User-friendly web interface for input and results.
+
+## Getting Started
+
+To use the Staff Promotion System, follow these steps:
+
+1. Clone this repository to your local machine or web server.
+
+2. Open the `index.html` file in a web browser to access the promotion calculator.
+
+## Usage
+
+1. **Fill in the Candidate Information:**
+   - Enter the candidate's name.
+   - Select the candidate's current rank from the dropdown menu.
+   - Provide activity points, performance points, and any special points.
+
+2. **Calculate Promotion Eligibility:**
+   - Click the "Calculate" button to calculate the candidate's promotion eligibility.
+
+3. **View Promotion Status:**
+   - The system will display a personalized promotion message indicating whether the candidate qualifies for promotion or not.
+
+4. **Download Promotion Status Text File:**
+   - A promotion status text file will be generated for the candidate.
+   - Click the provided download link to save the file.
+
+
+## Contributing
+
+Contributions are welcome! If you have any suggestions, bug reports, or feature requests, please open an issue or submit a pull request.
+
+
+## Acknowledgments
+
+- This system was originally created for Gamepixel and is now available for public use.
+
+---
+
+Feel free to update this README.md file with more details, installation instructions, screenshots, or any other information that would be helpful to users of your Staff Promotion System.
--- script.js
+++ script.js
@@ -1,81 +1,125 @@
+// Welcome to the Staff Promotion System
+// This system can be used to calculate and manage promotions for your organization.
+
+// Listen for the document to be fully loaded before initializing the system.
 document.addEventListener('DOMContentLoaded', function () {
-    document.getElementById('calculateButton').addEventListener('click', calculatePoints);
+    // Find the 'Calculate' button element by its ID.
+    const calculateButton = document.getElementById('calculateButton');
+    
+    // Check if the 'Calculate' button exists.
+    if (calculateButton) {
+        // Add a click event listener to the 'Calculate' button.
+        calculateButton.addEventListener('click', handlePromotionCalculation);
+    }
 });
 
-function calculatePoints() {
-    const candidateName = document.getElementById('candidateName').value;
-    const currentRank = document.getElementById('currentRank').value;
-    const activityPoints = parseInt(document.getElementById('activityPoints').value);
-    const performancePoints = parseInt(document.getElementById('performancePoints').value);
-    const specialPoints = parseInt(document.getElementById('specialPoints').value);
+// Function to handle the promotion calculation.
+function handlePromotionCalculation() {
+    // Retrieve input elements from the HTML.
+    const candidateNameInput = document.getElementById('candidateName');
+    const currentRankInput = document.getElementById('currentRank');
+    const activityPointsInput = document.getElementById('activityPoints');
+    const performancePointsInput = document.getElementById('performancePoints');
+    const specialPointsInput = document.getElementById('specialPoints');
+    const resultElement = document.getElementById('result');
+    resultElement.classList.add('show');
 
+    // Check if any of the required fields are empty.
+    if (
+        !candidateNameInput.value ||
+        !currentRankInput.value ||
+        !activityPointsInput.value ||
+        !performancePointsInput.value ||
+        !specialPointsInput.value
+    ) {
+        // Display a message to the user if any required fields are empty.
+        resultElement.textContent = 'Please fill in all required fields.';
+        return;
+    }
+
+    // Retrieve input values from the HTML.
+    const candidateName = candidateNameInput.value;
+    const currentRank = currentRankInput.value;
+    const activityPoints = parseInt(activityPointsInput.value);
+    const performancePoints = parseInt(performancePointsInput.value);
+    const specialPoints = parseInt(specialPointsInput.value);
+
+    // Define promotion-related data (starting points, weights, required points).
     const startingPoints = {
-        'Jr. Helper': 100,
-        'Helper': 200,
-        'GM': 400,
-        'Jr. Admin': 800,
-        'Admin': 1600
+        'Junior Staff': 100,
+        'Staff': 200,
+        'Senior Staff': 400,
+        'Moderator': 800,
+        'Administrator': 1600
     };
 
-    const weightAP = {
-        'Jr. Helper': 1,
-        'Helper': 1,
-        'GM': 1,
-        'Jr. Admin': 1,
-        'Admin': 1
+    const weightActivityPoints = {
+        'Junior Staff': 1,
+        'Staff': 1,
+        'Senior Staff': 1,
+        'Moderator': 1,
+        'Administrator': 1
     };
 
-    const weightPP = {
-        'Jr. Helper': 1,
-        'Helper': 1,
-        'GM': 1,
-        'Jr. Admin': 1,
-        'Admin': 1
+    const weightPerformancePoints = {
+        'Junior Staff': 1,
+        'Staff': 1,
+        'Senior Staff': 1,
+        'Moderator': 1,
+        'Administrator': 1
     };
 
     const requiredPointsForNextRank = {
-        'Jr. Helper': 200,
-        'Helper': 400,
-        'GM': 800,
-        'Jr. Admin': 1600,
-        'Admin': 3200
+        'Junior Staff': 200,
+        'Staff': 400,
+        'Senior Staff': 800,
+        'Moderator': 1600,
+        'Administrator': 3200
     };
 
+    // Calculate the total points based on the input.
     const totalPoints = startingPoints[currentRank] +
-        (activityPoints * weightAP[currentRank]) +
-        (performancePoints * weightPP[currentRank]) +
+        (activityPoints * weightActivityPoints[currentRank]) +
+        (performancePoints * weightPerformancePoints[currentRank]) +
         specialPoints;
 
-    const resultElement = document.getElementById('result');
+    // Initialize the promotion status message.
     let promotionStatus = '';
 
+    // Check if the candidate qualifies for promotion.
     if (totalPoints >= requiredPointsForNextRank[currentRank]) {
+        // Determine the next rank.
         const nextRank = getNextRank(currentRank);
+        // Generate a promotion message.
         promotionStatus = `Congratulations, ${candidateName}! You have been promoted to the rank of ${nextRank}. This is a significant achievement, and we appreciate your dedication and hard work. Your contributions have not gone unnoticed, and we believe you'll continue to excel in your new role.`;
     } else {
-        promotionStatus = `Sorry, ${candidateName}. We take our promotion process seriously, and your current performance and contributions do not meet the criteria for promotion at this time. However, this should not discourage you. We encourage you to keep working hard, improving your skills, and seeking opportunities to contribute positively to our community. With continued effort, you can achieve your goal of promotion in the future.`;
+        // Generate a message indicating that promotion criteria were not met.
+        promotionStatus = `Sorry, ${candidateName}. Based on your current performance and contributions, you do not meet the criteria for promotion at this time. However, this should not discourage you. We encourage you to keep working hard, improving your skills, and seeking opportunities to contribute positively to our organization. With continued effort, you can achieve your goal of promotion in the future.`;
     }
 
-    // Create a text file with the promotion status
+    // Create a text file with the promotion status and provide it to the user.
     createPromotionTextFile(candidateName, promotionStatus);
 
+    // Display the promotion status on the webpage.
     resultElement.textContent = promotionStatus;
 }
 
+// Function to determine the next rank in the hierarchy.
 function getNextRank(currentRank) {
-    const ranks = ['Jr. Helper', 'Helper', 'GM', 'Jr. Admin', 'Admin'];
+    const ranks = ['Junior Staff', 'Staff', 'Senior Staff', 'Moderator', 'Administrator'];
     const currentIndex = ranks.indexOf(currentRank);
     return currentIndex === -1 ? null : ranks[currentIndex + 1];
 }
 
+// Function to create a text file with the promotion status and trigger a download.
 function createPromotionTextFile(candidateName, message) {
-    const fileName = `${candidateName}.txt`;
+    const fileName = `${candidateName}_Promotion_Status.txt`;
     const fileContent = `Dear ${candidateName},\n\n${message}`;
 
-    // Create a Blob containing the file content
+    // Create a Blob containing the file content.
     const blob = new Blob([fileContent], { type: 'text/plain' });
 
-    // Create a download link and trigger a click event to download the file
+    // Create a download link and trigger a click event to download the file.
     const link = document.createElement('a');
     link.href = window.URL.createObjectURL(blob);
     link.download = fileName;
